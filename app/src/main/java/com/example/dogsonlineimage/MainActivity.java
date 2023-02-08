02130ee41e23611893e759fedbad904ffee54ff1
nikita <paramonov20@bk.ru>
2023-02-08 22:10:53 +0300

    glide

--- app/src/main/java/com/example/dogsonlineimage/MainActivity.java
+++ app/src/main/java/com/example/dogsonlineimage/MainActivity.java
@@ -2,21 +2,27 @@ package com.example.dogsonlineimage;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.bumptech.glide.Glide;
+import com.example.dogsonlineimage.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     private MainViewModel mainViewModel;
     private static final String TAG = "MainActivity";
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
         mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
         mainViewModel.loadDogImage();
@@ -24,8 +30,11 @@ public class MainActivity extends AppCompatActivity {
         mainViewModel.getDogImageLiveData().observe(this, new Observer<DogImage>() {
             @Override
             public void onChanged(DogImage dogImage) {
-                Log.d(TAG, dogImage.toString());
+                Glide.with(MainActivity.this)
+                        .load(dogImage.getMessage())
+                        .into(binding.imageView);
             }
         });
+
     }
 }
