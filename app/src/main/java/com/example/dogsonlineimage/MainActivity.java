39ffea4a31493f2aad7ce3824283e7f3b3e0f4f7
nikita <paramonov20@bk.ru>
2023-02-08 21:31:47 +0300

    refactoring

--- app/src/main/java/com/example/dogsonlineimage/MainActivity.java
+++ app/src/main/java/com/example/dogsonlineimage/MainActivity.java
@@ -4,58 +4,28 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String BASE_URL = "https://dog.ceo/api/breeds/image/random";
+    private MainViewModel mainViewModel;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loadDogImage();
-    }
 
-    private void loadDogImage() {
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
-        new Thread(new Runnable() {
+        mainViewModel.loadDogImage();
+
+        mainViewModel.getDogImageLiveData().observe(this, new Observer<DogImage>() {
             @Override
-            public void run() {
-                try {
-                    URL url = new URL(BASE_URL);
-                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                    InputStream inputStream = httpURLConnection.getInputStream();
-                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                    String result;
-
-                    StringBuilder data = new StringBuilder();
-                    do {
-                        result = bufferedReader.readLine();
-                        if (result != null) {
-                            data.append(result);
-                        }
-                    } while (result != null);
-
-                    JSONObject jsonObject = new JSONObject(data.toString());
-                    String status =  jsonObject.getString("status");
-                    String message = jsonObject.getString("message");
-
-                    DogImage dogImage = new DogImage(status, message);
-
-                    Log.d("MainActivity", dogImage.toString());
-                } catch (Exception e) {
-                    Log.d("MainActivity", e.toString());
-                }
+            public void onChanged(DogImage dogImage) {
+                Log.d(TAG, dogImage.toString());
             }
-        }).start();
+        });
     }
 }
